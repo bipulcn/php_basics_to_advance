5e7133a0b0b610358c79487342ef80ad5eba538b
bipul123 <bipul@nath.com>
2017-09-14 13:15:17 +0600

    basic upload 2

--- Basic_1/class_1.php
+++ Basic_1/class_1.php
@@ -1,3 +1,16 @@
-<?php
-echo "hello world";
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>PHP starts from here.</h1>
+    <?php
+		echo "hello world";  // basic PHP print line statement.
+		$name = "Your name"; // PHP variable Declaration 
+		echo "<br>"; // adding html content in PHP print statement.
+		echo $name;
+	?>
+  </body>
+</html>
