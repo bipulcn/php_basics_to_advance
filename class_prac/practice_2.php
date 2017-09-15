9a64c5b7de2d44d2449970a6f3b3fb682754ad4a
Bipul Chandra Dev Nath, mac <bipulnath@Bipuls-Mac-mini.local>
2017-09-15 20:02:13 +0600

    mac update 2

---
+++ class_prac/practice_2.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Class practice one: starting</title>
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+  </head>
+  <body>
+  	<div class="container">
+	    <h1>Object Oriented PHP Begins.</h1>
+	    <p class="note">We are trying to access some variable from PHP class. you know in class, variable are binded in the object.</p>
+      <p>We can access variable by calling the variable name next to object by -> symble. For static variable we are using the :: sign to access the variable.</p>
+      <p class="note">We will use the comple name that is like $variabel.</p>
+      <div class="phpOutput">
+        <?php
+        class manVariable
+        {
+          public static $var1 = 0;
+          function __construct()
+          {
+            echo "First calling variable: ".$this::$var1;
+            $this::$var1++;
+            echo "<br>Second time Calling Variable: ".$this::$var1;
+          }
+        }
+
+        $obj = new manVariable();
+        echo "<br>Access Public Variable: ".$obj::$var1;
+        ?>
+      </div>
+      <div class="phpCode">
+<?php
+$code = '<?php
+class manVariable
+{
+  public static $var1 = 0;
+  function __construct()
+  {
+    echo "First calling variable: ".$this::$var1;
+    $this::$var1++;
+    echo "<br>Second time Calling Variable: ".$this::$var1;
+  }
+}
+
+$obj = new manVariable();
+echo "<br>Access Public Variable: ".$obj::$var1;
+?>';
+echo highlight_string($code);
+?>
+      </div>
+      <p>Its so easy. Let us make it little complex so that it can make our work simple. </p>
+	</div>
+
+  </body>
+</html>
---
+++ class_prac/practice_3.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Class practice one: starting</title>
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+  </head>
+  <body>
+  	<div class="container">
+	    <h1>Object Oriented PHP Begins.</h1>
+	    <p class="note">Here we are trying to acess two kind of variable in two different way. They are only access by in this way.</p>
+      <div class="phpOutput">
+        <?php
+        class accessVariable
+        {
+          public static $var1 = 0;
+          public $var2 = 0;
+          function __construct()
+          {
+            echo "First calling variable: ".$this::$var1;
+            $this::$var1++;
+            echo "<br>Second time Calling Variable: ".$this::$var1;
+            echo "<br>Second Variable is: ".$this->var2;
+            $this->var2++;
+            echo "<br>Second time calling Second Variable: ".$this->var2;
+          }
+        }
+
+        $obj = new accessVariable();
+        echo "<br><br>Now from outside class";
+        echo "<br>Access Public first Variable: ".$obj::$var1;
+        echo "<br>Access Public second Variable: ".$obj->var2;
+        echo "<br><br>Now create another object<br>";
+        $obj2 = new accessVariable();
+        ?>
+      </div>
+      <div class="phpCode">
+<?php
+$code = '<?php
+class clsName
+{
+  function __construct()
+  {
+    echo "Class starts from here.";
+  }
+}
+
+$obj = new clsName();
+?>';
+echo highlight_string($code);
+?>
+      </div>
+      <p>Next try to create function</p>
+	</div>
+
+  </body>
+</html>
