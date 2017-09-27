bc45a6b64a3e6e69915b782ef34a78b01de4ae78
bipul123 <bipul@nath.com>
2017-09-27 10:32:51 +0600

    Basic Animations

--- mysql_logic/class_test.php
+++ mysql_logic/class_test.php
@@ -1,3 +1,26 @@
 <?php
+class parClass
+{	
+	function __construct()
+	{
+		echo "It was also called<br>";
+	}
+	function whyNotThis() {
+		echo "its not calling from child<br>";
+	}
+}
 
+class chiClass extends parClass
+{	
+	function __construct()
+	{
+		echo "is this was calling<br>";
+	}
+	function printIt() {
+		echo "its goit to be printed<br>";
+	}
+}
+$obj = new chiClass();
+$obj->printIt();
+$obj->whyNotThis();
 ?>
