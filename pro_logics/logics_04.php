8ebf8caef40b4b2b359a94a2604e98d89eae9c39
bipul <bipulcn@gmail.com>
2023-02-05 11:18:58 +0600

    updated on lab

--- pro_logics/logics_04.php
+++ pro_logics/logics_04.php
@@ -1,3 +1,4 @@
+<?php ini_set('display_errors', 1);?>
 <?php
 include('../php/getLinks.php');
 ?>
@@ -6,6 +7,36 @@ include('../php/getLinks.php');
 	    <p class="note"></p>
       <div class="phpOutput">
         <?php
+          $ary = array();
+          $ary[0] = "hello world";
+          $ary[1] = 100;
+          $ary[2] = 40;
+          $ary[3] = 'test';
+          $ary[4] = 10034;
+          echo "<pre>";
+          // var_dump($ary);
+          echo "</pre>";
+        ?>
+        <h3><?= $ary[0]; ?></h3>
+        <?php
+          for ($i=0; $i < sizeof($ary); $i++) {
+            echo $ary[$i]."<br>";
+          }
+          foreach ($ary as $key => $value) {
+            echo $key."= ".$value."<br>";
+          }
+        ?>
+
+        <hr>
+
+        <?php
+        $obj = new stdClass;
+        $obj->item1 = "this is test";
+
+        var_dump($ary);
+        echo "<br>";
+        var_dump($obj);
+        echo "<br>";
         ?>
       </div>
       <div class="phpCode">
